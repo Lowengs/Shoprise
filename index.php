a854b15788e3366070c6823cdda0af80d639af0f
Lowengs <blouiemichael@gmail.com>
2025-11-15 23:29:31 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SHOPRISE</title>
+    <!-- Bootstrap CSS -->
+    <link href="css/styles.css" rel="stylesheet">
+    <link rel="icon" href="pic/logonatinnobg.png" type="image/png">
+</head>
+<body style="background-color: #fef1e4;">
+    <div class="d-flex flex-column justify-content-center align-items-center min-vh-100 text-center">
+        <h2 class="font-HK text-briblo landingpg-text">
+            SHOP <span style="color:  #b48d00">RISE</span>
+        </h2>
+        <br>
+        <a href="guest/homepage.php" class="btn btn-outline-warning ms-2 button-custom">GET STARTED</a>
+    </div>
+    <footer class="font-HK p-3 text-white Bg-briblo justify-content-center align-items-center d-flex flex-column" style="border-top: 6px solid #fde49e;">
+        © Copyright SHOPPRISE 2024
+    </footer>
+    <script src="js/loading-screen.js"></script>
+</body>
+</html>
